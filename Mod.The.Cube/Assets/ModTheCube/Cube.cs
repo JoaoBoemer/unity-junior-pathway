8f1ad9cdf0c9aeb7980269a428c36be352277ad9
Jgcbitj <joao.boemer@edu.univali.br>
2024-08-06 23:22:35 -0300

    Mod.The.Cube and Prototype/Unit 3 Add
    Creating two new projects, mod.the.cube and prototype 3 to the unit 3

---
+++ Mod.The.Cube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        float redColor = Random.Range(0.0f, 1.0f);
+        float greenColor = Random.Range(0.0f, 1.0f);
+        float blueColor = Random.Range(0.0f, 1.0f);
+        float alphaColor = Random.Range(0.0f, 1.0f);
+
+        transform.localScale = Vector3.one * 2.0f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(redColor, greenColor, blueColor, alphaColor);
+
+        InvokeRepeating("MoveCube", 1.0f, 1.0f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 2.0f, 0.0f);
+    }
+
+    void MoveCube()
+    {
+        float xPos = Random.Range(0.0f, 5.0f);
+        float yPos = Random.Range(0.0f, 5.0f);
+        float zPos = Random.Range(0.0f, 5.0f);
+
+        transform.position = new Vector3(xPos, yPos, zPos);
+    }
+}
